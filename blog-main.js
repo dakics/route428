3d7f364834f9de9bd5f059934b9dd421f7b5bcfd
Srdjan Dakic <dakics@gmail.com>
2023-06-22 16:13:25 +0200

    Make more responsive layout on mobile

--- blog-main.js
+++ blog-main.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.querySelector('.sidebar');
+    const menuBtn = document.getElementById('menu-btn');
+    const content = document.querySelector('.content');
+
+    menuBtn.addEventListener('click', function() {
+        sidebar.classList.toggle('open');
+    });
+
+    content.addEventListener('click', function() {
+        if (sidebar.classList.contains('open')) {
+            sidebar.classList.remove('open');
+        }
+    });
+
     fetch('/assets/blog-posts.json')
         .then(response => response.json())
         .then(data => {
@@ -12,6 +26,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 postLink.textContent = post.title;
                 postLink.addEventListener('click', (e) => {
                     e.preventDefault();
+                    if (sidebar.classList.contains('open')) {
+                        sidebar.classList.remove('open');
+                    }
                     fetch(`/assets/${post.link}`)
                         .then(response => response.text())
                         .then(md => {
